7024a42c35f6ee9647ef321b7e809e491be085c8
Altaf Mahmud <altaf.mahmud@xfel.eu>
2024-10-03 04:37:54 +0200

    StackInSingleLinkedList: print popped value

--- CPP/StackInSingleLinkedList/src/main.cpp
+++ CPP/StackInSingleLinkedList/src/main.cpp
@@ -6,30 +6,33 @@ int main()
     std::cout << "Initial stack\n";
     stack.printStack();
     stack.printStack(true);
-    std::cout << "***************\n\n";
+    std::cout << "\n***************\n\n";
     stack.push(10);
     std::cout << "After pushing 10\n";
     stack.printStack();
     stack.printStack(true);
-    std::cout << "***************\n\n";
+    std::cout << "\n***************\n\n";
     stack.push(15);
     std::cout << "After pushing 15\n";
     stack.printStack();
     stack.printStack(true);
-    std::cout << "***************\n\n";
+    std::cout << "\n***************\n\n";
 
     stack.pop();
     std::cout << "After popping\n";
     stack.printStack();
     stack.printStack(true);
+    std::cout << "\n***************\n\n";
     stack.pop();
     std::cout << "After popping\n";
     stack.printStack();
     stack.printStack(true);
+    std::cout << "\n***************\n\n";
     stack.pop();
     std::cout << "After popping\n";
     stack.printStack();
     stack.printStack(true);
+    std::cout << "\n***************\n\n";
 
     return 0;
 }
