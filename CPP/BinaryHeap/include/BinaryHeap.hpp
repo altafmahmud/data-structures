349d6c8b6523e3e742f7e906f8c39bb472e4cfe3
Altaf Mahmud <altaf.mahmud@gmail.com>
2024-11-08 04:13:38 +0100

    BinaryHeap: throw exception for invalid swapping index

--- CPP/BinaryHeap/include/BinaryHeap.hpp
+++ CPP/BinaryHeap/include/BinaryHeap.hpp
@@ -135,7 +135,7 @@ private:
     {
         if (index1 == index2)
         {
-            return;
+            throw std::invalid_argument("1st Index: " + std::to_string(index1) + "2nd Index " + std::to_string(index2) + ": invalid for swapping\n");
         }
         auto temp = m_container[index1];
         m_container[index1] = m_container[index2];
